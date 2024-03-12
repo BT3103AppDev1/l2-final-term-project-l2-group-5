d02a5807978d35df3af8a8d5fd7ba9dff1a207f4
koiljat <koiljat@u.nus.edu>
2024-03-12 11:19:08 +0800

    added background image and input box

--- wefriends/src/views/Login.vue
+++ wefriends/src/views/Login.vue
@@ -1,6 +1,133 @@
 <template>
-    <h1>Login</h1>
-    <router-link to="/forgot">Forgot</router-link> |
-    <router-link to="/signup">Sign Up</router-link>
-    <router-view/>
+    <div id="left-half">
+        <img src="../assets/bckgrnd-img.png" alt="bckgrnd-img">
+    </div>
+    <div id="right-box">
+        <div id="brand">
+            <img src="../assets/logo.png" class="logo">
+            <h1 class="name">WeFriends</h1>
+        </div>
+        <h1 id="login-text">Login</h1>
+        <h2 id="login-text">We are glad to have you back here today.</h2>
+        <form @submit.prevent="submitForm">
+            <div class="rounded-input">
+                <input type="text" v-model="email" placeholder="Email">
+            </div>
+            <div class="rounded-input">
+                <input type="password" v-model="password" placeholder="Password">
+            </div>
+            <div id="forget-password">
+                <router-link to="/forgot">Forget Password?</router-link>
+            </div>
+            <button type="submit" id="login-button">Login</button>
+        </form>
+        <div class="signup-prompt">
+            Don't have an account? <router-link to="/signup" class="signup-link">Sign Up</router-link>
+        </div>        
+    </div>
 </template>
+
+
+<style scoped>
+#left-half {
+    position: absolute;
+    left: 0;
+    width: 50%;
+    height: 100%;
+    background-color: #436850;
+    padding: 20px;
+}
+
+#left-half img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    position: absolute;
+    top: 0;
+    left: 0;
+}
+
+#right-box {
+    position: absolute;
+    right: 0;
+    width: 50%;
+    height: 100%;
+    background-color: #FBFAF0;
+    padding: 20px;
+}
+
+.rounded-input input {
+    border-radius: 10px;
+    background-color: white;
+    width: 80%;
+    height: 30px;
+    padding: 10px;
+    border: 1px solid #ccc;
+    margin-bottom: 10px;
+    border-left: 10%;
+}
+
+.rounded-input {
+    margin-left: 10%;
+}
+
+#forget-password {
+    text-align: right;
+    margin-right: 15%;
+}
+
+#login-button {
+    background-color: #436850;
+    border: none; 
+    border-radius: 10px;
+    width: 20%;
+    height: 40px;
+    padding: 10px;
+    color: white;
+    text-align: center;
+    display: block;
+    margin: 10px auto; 
+    cursor: pointer; 
+}
+
+.signup-prompt {
+    display: flex;
+    justify-content: center;
+}
+
+.signup-link {
+    margin-left: 10px;
+}
+
+#login-text {
+    margin-left: 10%;
+}
+
+#brand {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+</style>
+
+
+<script>
+import firebaseApp from '@/firebase'
+import { getFirestore } from 'firebase/firestore'
+import { doc, setDoc } from 'firebase/firestore'
+
+export default {
+    data() {
+        return {
+            email: '',
+            password: '',
+        };
+    },
+    methods: {
+        submitForm() {
+            console.log("Form submitted with", this.email, this.password);
+        }
+    }
+}
+</script>
