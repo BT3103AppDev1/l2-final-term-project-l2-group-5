fe008a079961b98c480ec3e2401d0972b209b45f
edmundnwl <ngwledmund@gmail.com>
2024-03-18 17:44:22 +0800

    logout

---
+++ wefriends/src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <button @click="logout">Logout</button>
+</template>
+
+
+<script>
+import firebaseApp from "../firebase.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js"
+import { doc, setDoc } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js"
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-auth.js';
+
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
+
+export default {
+  data() {
+    return {
+      isLoggedIn: false
+    };
+  },
+  methods: {
+    logout() {
+        auth.signOut().then(() => {
+        console.log('Logged out');
+        // Redirect or perform some action upon successful logout
+        // this.$router.push({ name: 'login' });
+      }).catch((error) => {
+        console.error('Sign Out Error', error);
+      }); 
+    }
+  }
+}
+
+</script>
--- wefriends/src/views/Profile.vue
+++ wefriends/src/views/Profile.vue
@@ -1,3 +1,14 @@
 <template>
     <h1>Profile</h1>
+    <Navbar />
 </template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
