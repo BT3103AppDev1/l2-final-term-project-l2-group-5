98bfb2cf60837984459c4dcf1ce8ea3ee4543001
edmundnwl <ngwledmund@gmail.com>
2024-03-18 17:55:42 +0800

    logout redirect

--- wefriends/src/components/Navbar.vue
+++ wefriends/src/components/Navbar.vue
@@ -8,27 +8,31 @@ import firebaseApp from "../firebase.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js"
 import { doc, setDoc } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js"
 import { getAuth } from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-auth.js';
+import { useRouter } from 'vue-router';
 
 const auth = getAuth(firebaseApp);
 const db = getFirestore(firebaseApp);
 
 export default {
-  data() {
-    return {
-      isLoggedIn: false
-    };
-  },
-  methods: {
-    logout() {
-        auth.signOut().then(() => {
-        console.log('Logged out');
-        // Redirect or perform some action upon successful logout
-        // this.$router.push({ name: 'login' });
-      }).catch((error) => {
-        console.error('Sign Out Error', error);
-      }); 
+    data() {
+        return {
+            isLoggedIn: false
+        };
+    },
+    setup() {
+        const router = useRouter();
+        return { router };
+    },
+    methods: {
+        logout() {
+            auth.signOut().then(() => {
+                console.log('Logged out');
+                this.$router.push('/');
+            }).catch((error) => {
+                console.error('Sign Out Error', error);
+            });
+        }
     }
-  }
 }
 
 </script>
