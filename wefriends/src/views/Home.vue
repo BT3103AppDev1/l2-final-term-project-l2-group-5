2bde79e6c113d0e658163e38aff8d19c6ba92b57
briannoelk <e0958132@u.nus.edu>
2024-03-20 17:43:51 +0800

    Revert "initial set up"
    This reverts commit e5291de0571a86a010bd9a286038fd0f96f4ed43.

--- wefriends/src/views/Home.vue
+++ wefriends/src/views/Home.vue
@@ -1,91 +1,86 @@
 <template>
-    <div id="view">
-        <Navbar id="navbar" />
-        <div id="right-side">
-            <TopBar :pageName = "pageName" id="topbar"/>
-            <div id="content">
-                <div id="main">
-                    <div id="main-container">
-                        <div class="quote-container">
-                            <p class="quote">{{quote}}</p>
-                        </div>
-                        <div id="content-container">
-                            <div id="prompt-container">
-                                <h1>Today's Prompt</h1>
-                                <img src="../assets/diary-prompt-img.png" alt="Diary prompt image" class="prompt-img">
-                                <p id="prompt-title">{{prompt_title}}</p>
-                                <p id="prompt-desc">{{prompt_body}}</p>
-                                <div class="text-input" >
-                                    <form id="text-form">
-                                        <textarea placeholder="Type Here..."></textarea>
-                                        <button type="button" id="submit-button">></button>
-                                    </form>
-                                </div>
-                            </div>
-                            <div id="post-container">
-                                <div id="trending-post">
-                                    <div id="post-header">
-                                        <h1 id="heading-text">Trending Discussion </h1>
-                                        <select v-model="selectedOption" id="dropdown">
-                                            <option v-for="option in options" :value="option.value" :key="option.value">
-                                                {{ option.text }}
-                                            </option>
-                                        </select>
-                                    </div>
-                                    <ul>
-                                        <li v-for="(item, index) in topPosts" :key="index" class="post-title-box">
-                                            <p>{{ item.title }}</p>
-                                            {{ truncateText(item.body,300) }}
-                                        </li>
-                                    </ul>                        
-                                </div>
-                            </div>
-                        </div>
-                    </div>    
-                </div>
+    
+    <div id="main">
+        <div id="top-bar">
+            <h1>Home</h1>
+            <div id="brand">
+                <img src="../assets/logo.png" class="logo">
+                <h1 class="name">WeFriends</h1>
             </div>
         </div>
+        <div id="section">
+            <div class="quote-container">
+                <p class="quote">When you feel sad, just feel happy.</p>
+            </div>
+            <div id="content-container">
+                <div id="prompt-container">
+                    <h1>Today's Prompt</h1>
+                    <img src="../assets/diary-prompt-img.png" alt="Diary prompt image" class="prompt-img">
+                    <p id="prompt-title">Pet Power</p>
+                    <p id="prompt-desc">Did you know that having pets can increase our dopamine levels by over 70%? Write about your pet(s) or your dream pet!</p>
+                    <div class="text-input" >
+                        <form id="text-form">
+                            <textarea placeholder="Type Here..."></textarea>
+                            <button type="button" id="submit-button">></button>
+                        </form>
+                    </div>
+                </div>
+                <div id="post-container">
+                    <div id="trending-post">
+                        <div id="post-header">
+                            <h1 id="heading-text">Trending Discussion </h1>
+                            <select v-model="selectedOption" id="dropdown">
+                                <option v-for="option in options" :value="option.value" :key="option.value">
+                                    {{ option.text }}
+                                </option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>    
     </div>
-    
 </template>
 
 <style>
 
-#view {
+#brand {
     display: flex;
-    justify-content: space-around;
-    height: 100vh;
-}
-#navbar {
-    width: 20%;
-}
-#right-side {
-    width: 80%;
-    display: flex;
-    flex-direction: column;
-}
-#topbar {
-    height: 5%;
-}
-
-#content {
-    background-color: #ADBC9F;
-    height: 90%;
+    align-items: center;
+    justify-content: right;
+    margin-right: 2.5%;
 }
 
 #main {
-    position: flex;
+    position: absolute;
+    width: 85%;
+    height: 100%;
+    right: 0;
+}
+
+#top-bar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-left: 2.5%;
+}
+
+#section {
+    position: absolute;
+    right: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #adbc9f;
 }
 
 .quote-container {
     position: relative;
     max-width: 90%; 
     background-color: #fffee5; 
+    padding: 5px;
     border-radius: 25px; 
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); 
-    margin: auto; 
-    margin-top: 20px;
-    margin-bottom: 20px;
+    margin: 50px auto; 
     display: flex;
     align-items: center;
     justify-content: center;
@@ -102,6 +97,7 @@
 
 #content-container {
     display: flex;
+    justify-content: space-between;
 }
 
 #prompt-container {
@@ -131,6 +127,9 @@
 #post-container {
     flex: 2;
     margin-right: 5%;
+    padding: 20px;
+    background-color: #ADBC9F;
+    border-radius: 25px;
     text-align: center;
 }
 
@@ -141,12 +140,16 @@
 
 .text-input {
     display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-between;
     border-radius: 10px;
     border: 1px solid black;
 }
 
 .text-input textarea {
     background-color: white;
+    height: 60px;
 }
 
 #text-form {
@@ -164,7 +167,7 @@ textarea {
     outline: none; 
     padding-left: 10px; 
     padding-top: 10px;
-    font-family: Arial, sans-serif;
+    font-family: Arial, sans-serif; /* Change the font */
 }
 
 #submit-button {
@@ -188,6 +191,7 @@ textarea {
 textarea {
     overflow-wrap: break-word;
     resize: none; 
+    margin: 1%;
 }
 
 button {
@@ -202,33 +206,14 @@ button {
 
 #dropdown {
     padding: 10px 15px;
-    border-radius: 10px;
-    border: 1px solid #ccc;
-    background-color: #fff;
-}
-
-.post-title-box {
-    background-color: white;
-    padding: 10px 15px;
-    margin: 10px 0;
-    border: 1px solid #ccc;
-    border-radius: 8px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-    list-style-type: none;
-}
-
-ul {
-    padding-left: 0;
+    border-radius: 10px; /* Round the corners */
+    border: 1px solid #ccc; /* Add a border */
+    background-color: #fff; /* Change the background color */
 }
 
 </style>
 
 <script>
-import firebaseApp from '@/firebase'
-import { getFirestore, collection, getDocs, query, limit } from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js';
-import Navbar from '@/components/Navbar.vue'
-import TopBar from '@/components/TopBar.vue'
-
 export default {
     data() {
         return {
@@ -236,60 +221,10 @@ export default {
             options: [
                 { value: 'option1', text: 'Past 24 Hours' },
                 { value: 'option2', text: 'Past 1 Day' },
-            ],
-            topPosts: [],
-            pageName: "Home",
-            quote: "",
-            prompt_title: "",
-            prompt_body: ""
-        };
-    },
-    async mounted() {
-        const db = getFirestore(firebaseApp);
-
-        try {
-            const querySnapshot = await getDocs(query(collection(db, 'posts'), limit(3)));
-            querySnapshot.forEach((doc) => {
-                const postData = {
-                    body: doc.data().body,
-                    tag: doc.data().tag,
-                    timestamp: doc.data().timestamp.toDate(),
-                    title: doc.data().title,
-                };
-                this.topPosts.push(postData);
-            });
-        } catch (error) {
-            console.error(error);
-        }
-
-        try {
-            const querySnapshot = await getDocs(query(collection(db, 'quotes'), limit(1))); // apply some filter or some logic to get a new quote
-            querySnapshot.forEach((doc) => {
-                this.quote = doc.data().quote;
-            });
-        } catch (error) {
-            console.error(error);
-        }
-
-        try {
-            const querySnapshot = await getDocs(query(collection(db, 'prompts'), limit(1))); // apply some filter or some logic to get a new prompt
-            querySnapshot.forEach((doc) => {
-                this.prompt_title = doc.data().title;
-                this.prompt_body= doc.data().body;
-            });
-        } catch (error) {
-            console.error(error);
+                // Add more options as needed
+            ]
         }
     },
-    methods: {
-    truncateText(text, length) {
-      if (text.length <= length) return text;
-      return text.substring(0, length) + '...';
-        },
-    },
-    components: {
-        Navbar,
-        TopBar
-    }
+    // ...
 }
 </script>
